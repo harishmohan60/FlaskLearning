07657a681f7ca7ebdddb43efec116f2ba1d11f46
harishmohan60 <44602129+harishmohan60@users.noreply.github.com>
2021-01-11 12:15:43 +1300

    Flask Learning

--- Testing9/Basic.py
+++ Testing9/Basic.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, flash, session, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 
-app = Flask(__name__)
+app = Flask('__name__')
 
 app.config['SECRET_KEY'] = 'mykey'
 
@@ -16,11 +16,12 @@ def index():
     if form.validate_on_submit():
         flash('You just clicked the button!')
 
-        return redirect(url_for('index'))
-
+        return render_template('index.html', form=form)
+    
     return render_template('index.html', form=form)
-
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
